ae2725f8456c21df35dbb8f2a525057f8a6c2dbb
Ruth Fong <ruthcfong@gmail.com>
2019-05-30 18:43:54 +0100

    add script

--- scripts/05-30-19-save_localization_results.sh
+++ scripts/05-30-19-save_localization_results.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+python3 bbox_and_localization.py --attribution_method guided_backprop --data_dir /scratch/shared/slow/mandela/guided_backprop
+python3 bbox_and_localization.py --attribution_method gradient --data_dir /scratch/shared/slow/mandela/gradient
+
+python3 bbox_and_localization.py --attribution_method guided_backprop --data_dir /scratch/shared/slow/mandela/guided_backprop --method min_max_diff --small_range True
+python3 bbox_and_localization.py --attribution_method gradient --data_dir /scratch/shared/slow/mandela/gradient --method min_max_diff --small_range True
+
+python3 bbox_and_localization.py --attribution_method guided_backprop --data_dir /scratch/shared/slow/mandela/guided_backprop --method energy --small_range True
+python3 bbox_and_localization.py --attribution_method gradient --data_dir /scratch/shared/slow/mandela/gradient --method energy --small_range True
