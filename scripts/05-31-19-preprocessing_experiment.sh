1c264f981bfd8020baf3652af9022c83a8f0f190
Ruth Fong <ruthcfong@gmail.com>
2019-05-31 17:31:35 +0100

    add script to run bbox_and_localization in parallel

---
+++ scripts/05-31-19-preprocessing_experiment.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+smooths=( "0.0" "10.0" "20.0" )
+methods=( "mean" "min_max_diff" "energy" )
+processings=( "mean_crossover" "single_crossover" )
+
+for smooth in "${smooths[@]}"; do
+    for method in "${methods[@]}"; do
+        for processing in "${processings[@]}"; do
+            log_file="./logs/bbox_loc_log_${method}_${processing}_${smooth}.out"
+            echo "Launching job logging to ${log_file}"
+            python3 bbox_and_localization.py \
+                --method ${method} \
+                --processing ${processing} \
+                --smooth ${smooth} &> ${log_file} &
+        done
+    done
+done
+
